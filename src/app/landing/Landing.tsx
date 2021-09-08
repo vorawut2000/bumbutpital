73b0dd3c270ae189c501e569d85a1bfce7a212fe
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-09-09 02:20:59 +0700

    [FIX] style in landing

--- src/app/landing/Landing.tsx
+++ src/app/landing/Landing.tsx
@@ -5,11 +5,11 @@ import classes from "./Landing.module.css";
 const useStyles = makeStyles({
   root: {
     background: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-    borderRadius: 3,
+    borderRadius: 12,
     border: 0,
     color: "white",
     width: 200,
-    height: 48,
+    height: 52,
     padding: "0 30px",
     boxShadow: "0 3px 5px 2px rgba(255, 105, 135, .3)",
   }
