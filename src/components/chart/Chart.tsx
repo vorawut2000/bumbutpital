36b5205908c6240daa90aa7a9ee520f5e284d303
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-09-11 01:16:17 +0700

    [REFRACTOR] Chart in dashboard

--- src/components/chart/Chart.tsx
+++ src/components/chart/Chart.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 import classes from "./Chart.module.css";
+import GaugeChart from "react-gauge-chart";
 
 const Chart = () => {
+  const chartStyle = {
+    height: 243,
+    width: 540,
+  };
+
   return (
     <div className={classes.chart}>
-      <h3 className="chartTitle">Chart ทำไงวะไอเหี้ยเย็ดแม่</h3>
+      <h3 className={classes.chartTitle}>Average depression of all users</h3>
+      <GaugeChart
+        nrOfLevels={5}
+        colors={["#20CBFE", "#32D475", "#FFBC17", "#E76849", "#F14949"]}
+        arcWidth={0.1}
+        percent={0.40}
+        cornerRadius={4}
+        arcPadding={0}
+        style={chartStyle}
+        className={classes.gaugeChart}
+        textColor= "#00000"
+      />
     </div>
   );
 };
