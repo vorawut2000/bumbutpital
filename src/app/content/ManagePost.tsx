87fc010bb541b92e0cb401b76cd4cc86ac35e471
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-09-11 15:19:37 +0700

    [REFACTOR] 🎉Manage Post page

--- src/app/content/ManagePost.tsx
+++ src/app/content/ManagePost.tsx
@@ -1,59 +1,86 @@
 import React from "react";
 import { DataGrid } from "@mui/x-data-grid";
 import { DeleteOutline } from "@material-ui/icons";
-import { userRows } from "../../dummyData";
-import { Link } from "react-router-dom";
+import { contentData } from "../../dummyData";
+import { Link, useHistory } from "react-router-dom";
 import { useState } from "react";
-import "./ManagePost.css";
+import classes from "./ManagePost.module.css";
+import { Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  root: {
+    background: "#6367EA",
+    borderRadius: 5,
+    border: 0,
+    color: "white",
+    height: 36,
+    float: "right",
+  },
+});
 
 //have to change type!!!!
 const ManagePost = () => {
-  const [data, setData] = useState(userRows);
+  const style = useStyles();
+  const history = useHistory();
+  const [data, setData] = useState(contentData);
 
   const handleDelete = (id: any) => {
     setData(data.filter((item) => item.id !== id));
   };
 
+  const submitHandler = () => {
+    history.push("/home");
+  };
+
   const columns = [
-    { field: "id", headerName: "ID", width: 90 },
+    { field: "id", headerName: "No.", width: 190 },
     {
-      field: "user",
-      headerName: "User",
-      width: 200,
-      renderCell: (params: any) => {
-        return (
-          <div className="userListUser">
-            <img className="userListImg" src={params.row.avatar} alt="" />
-            {params.row.username}
-          </div>
-        );
-      },
+      field: "title",
+      headerName: "Title",
+      width: 300,
     },
-    { field: "email", headerName: "Email", width: 200 },
+    { field: "username", headerName: "Author", width: 300 },
     {
-      field: "status",
-      headerName: "Status",
-      width: 120,
+      field: "categories",
+      headerName: "Categories",
+      width: 300,
     },
     {
-      field: "transaction",
-      headerName: "Transaction Volume",
-      width: 160,
+      field: "depression",
+      headerName: "Depression Severity",
+      width: 300,
+    },
+    {
+      field: "date",
+      headerName: "Date",
+      width: 300,
     },
     {
       field: "action",
       headerName: "Action",
-      width: 150,
+      width: 300,
       renderCell: (params: any) => {
         return (
           <>
-            <Link to={"/user/" + params.row.id}>
-              <button className="userListEdit">Edit</button>
+            <Link
+              to={"/posts/" + params.row.id}
+              className={classes.userListEdit}
+            >
+              Edit
             </Link>
-            <DeleteOutline
-              className="userListDelete"
+            <Link
+              to={"/posts/" + params.row.id}
+              className={classes.userListDelete}
               onClick={() => handleDelete(params.row.id)}
-            />
+            >
+              Delete
+            </Link>
+            <Link
+              to={"/posts/" + params.row.id}
+              className={classes.userListPublish}
+            >
+              Publish
+            </Link>
           </>
         );
       },
@@ -61,13 +88,27 @@ const ManagePost = () => {
   ];
 
   return (
-    <div className="userList">
+    <div className={classes.userList}>
+      <div className={classes.userTitle}>
+        Manage Post
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          className={style.root}
+          onClick={submitHandler}
+        >
+          Add Post
+        </Button>
+      </div>
       <DataGrid
         rows={data}
         disableSelectionOnClick
         columns={columns}
-        pageSize={8}
+        pageSize={10}
         checkboxSelection
+        autoPageSize={true}
+        autoHeight
       />
     </div>
   );
